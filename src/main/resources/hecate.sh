e26d2b5f5e3827308caaf004abc52de3f4abe221
Kimberly <mrkid863@gmail.com>
2023-02-19 12:06:11 +0800

    correct logback-spring & update otel resource attributes in shell script

--- src/main/resources/hecate.sh
+++ src/main/resources/hecate.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec java -javaagent:opentelemetry-javaagent.jar -Dspring.profiles.active=jsonlog -Dotel.exporter.otlp.endpoint=${otlp_endpoint:-http://localhost:4317} -Dotel.service.name=${project.artifactId}-${project.version} -cp "lib/:lib/*:ext/:ext/*" org.kquiet.hecate.Launcher
+exec java -javaagent:opentelemetry-javaagent.jar -Dspring.profiles.active=jsonlog -Dotel.exporter.otlp.endpoint=${otlp_endpoint:-http://localhost:4317} -Dotel.resource.attributes=service.name=${project.artifactId},service.version=${project.version} -cp "lib/:lib/*:ext/:ext/*" org.kquiet.hecate.Launcher
