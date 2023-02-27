9c921c4c57f14397800b6dc072d884c05d0acabd
Kimberly <mrkid863@gmail.com>
2023-02-27 19:24:42 +0800

    remove download of opentelemetry java agent library

--- src/main/resources/hecate.sh
+++ src/main/resources/hecate.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec java -javaagent:opentelemetry-javaagent.jar -Dspring.profiles.active=jsonlog -Dotel.exporter.otlp.endpoint=${otlp_endpoint:-http://localhost:4317} -Dotel.resource.attributes=service.name=${project.artifactId},service.version=${project.version} -cp "lib/:lib/*:ext/:ext/*" org.kquiet.hecate.Launcher
+exec java -Dspring.profiles.active=jsonlog -cp "lib/:lib/*:ext/:ext/*" org.kquiet.hecate.Launcher
